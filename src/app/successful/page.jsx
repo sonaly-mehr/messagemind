4121a22cfe1fccb4e9c8c7f19e76c811f8ef0ee2
sonaly-mehr <sonaly.mehr@gmail.com>
2024-02-16 16:39:34 +0600

    test

--- src/app/successful/page.jsx
+++ src/app/successful/page.jsx
@@ -1,22 +1,21 @@
 "use client";
 import React, { useEffect } from "react";
 import HalfColoredContainer from "@/components/ui/HalfColoredContainer";
-import { Checkmark } from "react-checkmark";
 import SubmitButton from "@/components/ui/Button";
 import Link from "next/link";
 
 const SuccessfulPage = () => {
-  useEffect(()=> {
-    if (typeof window !== 'undefined') {
-      console.log('window.innerHeight', window.innerHeight);
-  }
-  }, [])
+  // useEffect(()=> {
+  //   if (typeof window !== 'undefined') {
+  //     console.log('window.innerHeight', window.innerHeight);
+  // }
+  // }, [])
   return (
     <HalfColoredContainer>
       <div className="w-[90%] md:w-2/5 m-auto h-screen flex flex-col justify-center relative top-0 md:top-[-50px] z-30">
         <div className="w-full">
           <div className="box_shaddow bg-white border-[1px] border-solid border-[#F2F4F5] rounded-2xl px-7 md:px-16 pt-10 md:pt-14 pb-6">
-            <Checkmark size="xLarge" />
+            {/* <CheckMark size="xLarge" /> */}
             <h2 className="heading text-center mt-8">
               Completed Successfully!
             </h2>
